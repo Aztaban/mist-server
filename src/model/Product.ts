49ffbe8212286e7aecd88165a63ccb25d74648cf
Martin Justa <Aztabanee@gmail.com>
2024-03-26 00:51:43 +0100

    add product model

---
+++ src/model/Product.ts
@@ -0,0 +1,49 @@
+import mongoose, { Schema, Document } from 'mongoose';
+
+export interface Product extends Document {
+  id: string;
+  name: string;
+  productType: string;
+  price?: number;
+  image: string;
+  details: {
+    author?: string;
+    releaseDate: string;
+    description: string;
+  };
+}
+
+const productSchema: Schema = new Schema({
+  id: {
+    type: String,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  productType: {
+    type: String,
+    required: true,
+  },
+  price: String,
+  image: {
+    type: String,
+    required: true,
+  },
+  details: {
+    author: {
+      type: String,
+      required: true,
+    },
+    releaseDate: {
+      type: String,
+      default: 'TBD Soon',
+    },
+    description: String,
+  },
+});
+
+const ProductModel = mongoose.model<Product>('Product', productSchema);
+
+export default ProductModel;
