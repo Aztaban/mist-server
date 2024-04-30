75b481102557d269a367c73b468da5c5fc32b3d3
Martin Justa <Aztabanee@gmail.com>
2024-04-30 15:19:46 +0200

    modified to save error logs in log file instead of txt

--- src/middleware/errorHandler.ts
+++ src/middleware/errorHandler.ts
@@ -2,7 +2,7 @@ import { logEvents } from "./logEvents";
 import { Request, Response,NextFunction } from "express";
 
 const errorHandler = (err: Error, req: Request, res: Response, next: NextFunction) => {
-  logEvents(`${err.name}: ${err.message}`, 'errLog.txt');
+  logEvents(`${err.name}: ${err.message}`, 'errLog.log');
   console.error(err.stack);
   res.status(500).send(err.message);
 }
