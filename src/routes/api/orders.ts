0c56d9a9a4ae79f02842d2281f1e40b8f870262b
Martin Justa <Aztabanee@gmail.com>
2024-05-30 23:11:07 +0200

    Fix. Changed order of routes to access route /user

--- src/routes/api/orders.ts
+++ src/routes/api/orders.ts
@@ -5,18 +5,19 @@ import ordersController from '../../controllers/ordersController';
 const router: Router = express.Router();
 
 router
-  .route('/') 
-  .get(ordersController.getAllOrders)  // Get all Orders
-  .post(ordersController.createNewOrder); // Create a new Order 
+  .route('/')
+  .get(ordersController.getAllOrders) // Get all Orders
+  .post(ordersController.createNewOrder); // Create a new Order
+
+router.route('/user').get(ordersController.getOrdersForUser); // get orders for user who requested it
 
 router
   .route('/:id')
-  .get(ordersController.getOrderById)  // Get Order by ID
+  .get(ordersController.getOrderById) // Get Order by ID
   .put(ordersController.updateOrder) // Update Order
-  .delete(ordersController.deleteOrder) // Delete Order
+  .delete(ordersController.deleteOrder); // Delete Order
 
-router.route('/:id/status').put(ordersController.updateOrderStatus) // Update Order Status
-router.route('/:id/pay').put(ordersController.updateOrderPaid) // Mark Order as Paid
-//router.route('/:status').get() // Get Orders by Status
+router.route('/:id/status').put(ordersController.updateOrderStatus); // Update Order Status
+router.route('/:id/pay').put(ordersController.updateOrderPaid); // Mark Order as Paid
 
 export = router;
