e2fb0c9b3164ed4872fa07b1d1c4720cc363c261
Martin Justa <Aztabanee@gmail.com>
2024-09-24 16:32:32 +0200

    Added JWT verification to private routes.

--- src/routes/api/posts.ts
+++ src/routes/api/posts.ts
@@ -2,6 +2,7 @@ import express, { Router } from 'express';
 import verifyRoles from '../../middleware/verifyRoles';
 import { ROLES_LIST } from '../../config/roles_list';
 import postsController from '../../controllers/postsController';
+import verifyJWT from '../../middleware/verifyJWT';
 
 const router: Router = express.Router();
 
@@ -9,15 +10,20 @@ router
   .route('/')
   .get(postsController.getAllPosts)
   .post(
+    verifyJWT,
     verifyRoles(ROLES_LIST.Admin, ROLES_LIST.Editor),
     postsController.createNewPost
   )
-  .delete(verifyRoles(ROLES_LIST.Admin), postsController.deletePost);
+  .delete(
+    verifyJWT,
+    verifyRoles(ROLES_LIST.Admin), 
+    postsController.deletePost);
 
 router
   .route('/:id')
   .get(postsController.getPostById)
   .put(
+    verifyJWT,
     verifyRoles(ROLES_LIST.Admin, ROLES_LIST.Editor),
     postsController.updatePost
   );
