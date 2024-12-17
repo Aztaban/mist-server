9041434c49b58ef830beff46d708431b87c65f6d
Martin Justa <Aztabanee@gmail.com>
2024-12-17 15:54:12 +0100

    feat: add route for payment intent.

--- src/routes/api/orders.ts
+++ src/routes/api/orders.ts
@@ -1,6 +1,7 @@
 import express, { Router } from 'express';
 import verifyRoles from '../../middleware/verifyRoles';
 import ordersController from '../../controllers/ordersController';
+import { createPaymentIntent } from '../../controllers/paymentController';
 import { ROLES_LIST } from '../../config/roles_list';
 
 const router: Router = express.Router();
@@ -20,6 +21,7 @@ router
 
 router.route('/:id/status').put(verifyRoles(ROLES_LIST.Editor, ROLES_LIST.Admin),ordersController.updateOrderStatus); // Update Order Status
 router.route('/:id/shipping').put(verifyRoles(ROLES_LIST.Editor, ROLES_LIST.Admin),ordersController.updateOrderShipping); // Update Shipping method
-router.route('/:id/pay').put(ordersController.updateOrderPaid); // Mark Order as Paid
+router.route('/:id/mark-paid').put(ordersController.updateOrderPaid) // Mark Order as Paid
+router.route('/:id/payment-intent').post(createPaymentIntent);
 
 export = router;
