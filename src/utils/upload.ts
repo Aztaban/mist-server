1e3512b50a137c1ebc1bdee3940079f8b8fb2b30
Martin Justa <Aztabanee@gmail.com>
2025-02-13 19:24:39 +0100

    feat: added file filter.

--- src/utils/upload.ts
+++ src/utils/upload.ts
@@ -3,16 +3,40 @@ import path from 'path';
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, path.join(__dirname, '../../public/uploads/images'))
+    cb(null, path.join(__dirname, '../../public/uploads/images'));
   },
   filename: (req, file, cb) => {
     const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
     cb(null, uniqueSuffix + '-' + file.originalname);
+  },
+});
+
+const fileFilter = (
+  req: any,
+  file: Express.Multer.File,
+  cb: multer.FileFilterCallback
+) => {
+  const allowedTypes = /jpeg|jpg|png|gif|webp/;
+  const extensionName = allowedTypes.test(
+    path.extname(file.originalname).toLowerCase()
+  );
+  const mimeType = allowedTypes.test(file.mimetype);
+
+  if (extensionName && mimeType) {
+    return cb(null, true);
+  } else {
+    cb(
+      new Error(
+        'Invalid file type. Only JPEG, JPG, PNG, GIF, and WebP files are allowed.'
+      )
+    );
   }
-})
+};
 
-const upload = multer({ storage });
+const upload = multer({
+  storage,
+  limits: { fileSize: 2 * 1024 * 1024 },
+  fileFilter,
+});
 
-const singleFileUpload = upload.single('image');
-
-export { singleFileUpload };
+export const singleFileUpload = upload.single('image');
