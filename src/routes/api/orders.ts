ac5cfe9f827cfad7924d27db9414f8f08392b5db
Martin Justa <Aztabanee@gmail.com>
2025-02-26 17:17:35 +0100

    reafactor: new import and removed routes

--- src/routes/api/orders.ts
+++ src/routes/api/orders.ts
@@ -1,6 +1,6 @@
 import express, { Router } from 'express';
 import verifyRoles from '../../middleware/verifyRoles';
-import ordersController from '../../controllers/ordersController';
+import * as ordersController from '../../controllers/orders';
 import { createPaymentIntent } from '../../controllers/paymentController';
 import { ROLES_LIST } from '../../config/roles_list';
 
@@ -8,20 +8,29 @@ const router: Router = express.Router();
 
 router
   .route('/')
-  .get(verifyRoles(ROLES_LIST.Editor, ROLES_LIST.Admin),ordersController.getAllOrders) // Get all Orders
-  .post(ordersController.createNewOrder); // Create a new Order
+  .get(
+    verifyRoles(ROLES_LIST.Editor, ROLES_LIST.Admin),
+    ordersController.getAllOrders
+  )
+  .post(ordersController.createNewOrder);
 
-router.route('/user').get(verifyRoles(ROLES_LIST.User),ordersController.getOrdersForUser); // get orders for user who requested it
+router
+  .route('/user')
+  .get(verifyRoles(ROLES_LIST.User), ordersController.getOrdersForUser); // get orders for user who requested it
 
 router
   .route('/:id')
-  .get(verifyRoles(ROLES_LIST.User ,ROLES_LIST.Editor, ROLES_LIST.Admin),ordersController.getOrderById) // Get Order by ID
-  .put(verifyRoles(ROLES_LIST.Editor, ROLES_LIST.Admin),ordersController.updateOrder) // Update Order
-  .delete(verifyRoles(ROLES_LIST.Admin),ordersController.deleteOrder); // Delete Order
+  .get(
+    verifyRoles(ROLES_LIST.User, ROLES_LIST.Editor, ROLES_LIST.Admin),
+    ordersController.getOrderById
+  )
+  .put(
+    verifyRoles(ROLES_LIST.Editor, ROLES_LIST.Admin),
+    ordersController.updateOrder
+  )
+  .delete(verifyRoles(ROLES_LIST.Admin), ordersController.deleteOrder);
 
-router.route('/:id/status').put(verifyRoles(ROLES_LIST.Editor, ROLES_LIST.Admin),ordersController.updateOrderStatus); // Update Order Status
-router.route('/:id/shipping').put(verifyRoles(ROLES_LIST.Editor, ROLES_LIST.Admin),ordersController.updateOrderShipping); // Update Shipping method
-router.route('/:id/mark-paid').put(ordersController.updateOrderPaid) // Mark Order as Paid
+router.route('/:id/mark-paid').put(ordersController.updateOrderPaid);
 router.route('/:id/payment-intent').post(createPaymentIntent);
 
 export = router;
