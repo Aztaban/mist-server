831326250bcd893b83d550b3911da543f343787d
Martin Justa <Aztabanee@gmail.com>
2025-02-28 16:48:25 +0100

    refactor: new services added

--- src/services/userServices.ts
+++ src/services/userServices.ts
@@ -1,21 +1,43 @@
-import UserModel, { User } from '../models/User';
+import UserModel, { User, Address } from '../models/User';
+import { Types } from 'mongoose';
+import { ROLES_LIST } from '../config/roles_list';
 
-export const findUserByUsername = async (username: string): Promise<User | null> => {
+export const findUserByUsername = async (
+  username: string
+): Promise<User | null> => {
   return await UserModel.findOne({ username }).exec();
 };
 
-export const findUserByRefreshToken = async (refreshToken: string): Promise<User | null> => {
+export const findUserById = async (id: string) => {
+  return await UserModel.findById(id).exec();
+};
+
+export const findUserByEmail = async (email: string) => {
+  return await UserModel.findOne({ email }).exec();
+};
+
+export const findUserByRefreshToken = async (
+  refreshToken: string
+): Promise<User | null> => {
   return await UserModel.findOne({ refreshToken }).exec();
 };
 
-export const createUser = async (username: string, hashedPassword: string): Promise<User> => {
+export const createUser = async (
+  username: string,
+  email: string,
+  password: string
+): Promise<User> => {
   return await UserModel.create({
     username,
-    password: hashedPassword,
+    email,
+    password,
   });
 };
 
-export const updateUserRefreshToken = async (user: User, refreshToken: string): Promise<User> => {
+export const updateUserRefreshToken = async (
+  user: User,
+  refreshToken: string
+): Promise<User> => {
   user.refreshToken = refreshToken;
   return await user.save();
 };
@@ -23,4 +45,46 @@ export const updateUserRefreshToken = async (user: User, refreshToken: string): 
 export const clearUserRefreshToken = async (user: User): Promise<User> => {
   user.refreshToken = '';
   return await user.save();
+};
+
+export const updateUserContactInfo = async (
+  userId: Types.ObjectId,
+  address?: Address,
+  phoneNumber?: string
+): Promise<User> => {
+  const user = await UserModel.findById(userId);
+  if (!user) {
+    throw new Error('User not found');
+  }
+  if (address) {
+    user.address = address;
+  }
+  if (phoneNumber) {
+    user.phoneNumber = phoneNumber;
+  }
+  return await user.save();
+};
+
+export const updateUserStatus = async (id: string, isActive: boolean) => {
+  return await UserModel.findByIdAndUpdate(id, { isActive }, { new: true });
+};
+
+export const toggleEditorRole = async (id: string) => {
+  const user = await UserModel.findById(id);
+  if (!user) return null;
+
+  const editorRole = ROLES_LIST.Editor;
+
+  if (user.roles.includes(editorRole)) {
+    user.roles = user.roles.filter(role => role !== editorRole);
+  } else {
+    user.roles.push(editorRole);
+  }
+
+  await user.save();
+  return user;
+};
+
+export const findAllUsers = async (): Promise<User[]> => {
+  return await UserModel.find().select("-password").exec();
 };
