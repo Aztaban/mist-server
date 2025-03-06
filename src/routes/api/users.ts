a9f8ae2e132a3377bae13becfd4c3c77bf392bdc
Martin Justa <Aztabanee@gmail.com>
2025-03-06 20:49:26 +0100

    feat: added route for user orders

--- src/routes/api/users.ts
+++ src/routes/api/users.ts
@@ -1,12 +1,14 @@
 import express, { Router } from 'express';
 import verifyRoles from '../../middleware/verifyRoles';
 import * as userController from '../../controllers/users';
+import { getOrdersForUser } from '../../controllers/orders';
 import { ROLES_LIST } from '../../config/roles_list';
 
 const router: Router = express.Router();
 
 router.route('/').get(userController.getUser);
 
+router.route('/orders').get(getOrdersForUser);
 router.route('/address').put(userController.updateUserAddressAndPhone);
 router
   .route('/admin')
