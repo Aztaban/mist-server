b3944ac540b6981a9da3cf2b29858ee9269512ac
Martin Justa <Aztabanee@gmail.com>
2025-03-12 01:40:05 +0100

    feat: added regex config file.

---
+++ src/config/regex.ts
@@ -0,0 +1,4 @@
+export const USER_REGEX = /^[A-z][A-z0-9-_]{3,23}$/;
+export const EMAIL_REGEX = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+export const PWD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8,24}$/;
+export const PHONE_REGEX = /^\+?\d{10,15}$/;
