9426acfeca6c4cc90d5b8b196fdbfd6e2715612b
Martin Justa <Aztabanee@gmail.com>
2025-04-01 22:10:23 +0200

    feat: added comment

--- src/routes/root.ts
+++ src/routes/root.ts
@@ -3,6 +3,15 @@ import path from 'path';
 
 const router: Router = express.Router();
 
+/**
+ * Route handler for the homepage.
+ * This route matches either:
+ *  - the root path `/`
+ *  - `/index`
+ *  - `/index.html`
+ * When one of these paths is accessed via a GET request,
+ * it responds by sending the `index.html` file located in the `views` directory.
+ */
 router.get('^/$|/index(.html)?', (req: Request, res: Response): void => {
   res.sendFile(path.join(__dirname, '..', 'views', 'index.html'));
 });
