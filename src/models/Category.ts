5b30b1ef8a0129ba79e663fe0701613570f526fa
Aztaban <Aztabanee@gmail.com>
2025-05-16 03:33:46 +0200

    Add virtual id to category

--- src/models/Category.ts
+++ src/models/Category.ts
@@ -23,6 +23,13 @@ categorySchema.virtual('id').get(function (this: Document) {
   return this._id as ObjectId;
 });
 
+categorySchema.set('toJSON', {
+  virtuals: true,
+  versionKey: false,
+  transform: (_, ret) => {
+    delete ret._id;
+  },
+});
 const Category: Model<ICategory> = mongoose.model<ICategory>('Category', categorySchema);
 
 export default Category;
