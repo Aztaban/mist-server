5dcbab92795621f50056a786c901a2052551b308
Aztaban <Aztabanee@gmail.com>
2025-05-16 02:58:01 +0200

    fix: added verifyJWT middleware for protected paths in category

--- src/routes/api/categories.ts
+++ src/routes/api/categories.ts
@@ -2,12 +2,17 @@ import express, { Router } from 'express';
 import * as categoryController from '../../controllers/category';
 import verifyRoles from '../../middleware/verifyRoles';
 import { ROLES_LIST } from '../../config/roles_list';
+import verifyJWT from '../../middleware/verifyJWT';
 
 const router: Router = express.Router();
 
 router.route('/').get(categoryController.getCategories);
-router.route('/').post(verifyRoles(ROLES_LIST.Admin, ROLES_LIST.Editor), categoryController.addCategory);
-router.route('/:id').delete(verifyRoles(ROLES_LIST.Admin, ROLES_LIST.Editor), categoryController.removeCategory);
-router.route('/:id').patch(verifyRoles(ROLES_LIST.Admin, ROLES_LIST.Editor), categoryController.updateCategory);
+router.route('/').post(verifyJWT, verifyRoles(ROLES_LIST.Admin, ROLES_LIST.Editor), categoryController.addCategory);
+router
+  .route('/:id')
+  .delete(verifyJWT, verifyRoles(ROLES_LIST.Admin, ROLES_LIST.Editor), categoryController.removeCategory);
+router
+  .route('/:id')
+  .patch(verifyJWT, verifyRoles(ROLES_LIST.Admin, ROLES_LIST.Editor), categoryController.updateCategory);
 
 export = router;
